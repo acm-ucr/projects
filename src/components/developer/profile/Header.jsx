51d053f1b7ecfeb13e165ece6f4a1391931581b4
Yash Kathe <yashkathe123@gmail.com>
2024-01-18 22:52:37 -0800

    used Image tag, changed  bg color, used height and width defaults and using local image

--- src/components/developer/profile/Header.jsx
+++ src/components/developer/profile/Header.jsx
@@ -1,15 +1,20 @@
+import Image from "next/image";
+
+import pfp from "@/public/images/pfp.jpg";
+
 const Header = () => {
   return (
-    <div className="h-1/6 w-full bg-fuchsia-950 flex flex-row justify-center items-center px-6 py-1">
-      <div className="w-[10%]">
-        <img
+    <div className="h-1/6 w-full bg-purple-950 flex flex-row justify-center items-center px-6 py-1">
+      <div className="w-40">
+        <Image
           className="rounded-md  h-full aspect-square"
-          src="https://as1.ftcdn.net/v2/jpg/01/63/11/70/1000_F_163117064_syJkTuCddASYjvl4WqyRmnuy8cDXpoQY.jpg"
-        ></img>
+          src={pfp}
+          alt="profile picture"
+        ></Image>
       </div>
-      <div className="w-[65%] h-[80%] px-2 flex flex-col align-start justify-center">
+      <div className="w-full h-auto px-2 flex flex-col align-start justify-center">
         <div className="flex flex-row items-center gap-4 text-center">
-          <h3 className="text-2xl font-bold text-white">Evan Little Cat</h3>
+          <h3 className="text-3xl font-bold text-white">Evan Little Cat</h3>
           <p className="bg-blue-400 h-fit rounded-full px-4 font-semibold text-center">
             developer
           </p>
@@ -18,19 +23,19 @@ const Header = () => {
           evan213@ucr.edu
         </p>
         <div className="flex flex-row align-center mt-8">
-          <h5 className="text-white font-semibold">past projects:</h5>
-          <p className="border-2 font rounded-full text-red-400 border-red-400 px-4 py-0 mx-1 ">
+          <h5 className="text-white font-semibold text-sm">past projects:</h5>
+          <p className="border-2 font rounded-full text-red-400 border-red-400 px-4 py-0 ml-2 text-sm">
             IEEE Website
           </p>
-          <p className="border-2 font rounded-full text-green-700 border-green-700 px-4 py-0 mx-1 ">
+          <p className="border-2 font rounded-full text-green-700 border-green-700 px-4 py-0 ml-2 text-sm">
             PAD Website
           </p>
         </div>
       </div>
-      <div className="w-[25%] h-[80%] flex flex-row justify-end mt-4">
-        <h3 className="text-white font-semibold">current project: </h3>
-        <p className="border-2 font rounded-full h-fit text-red-400 border-red-400 px-4 py-0 mx-1 ">
-          IEEE Website
+      <div className="w-80 h-1/2 flex flex-row mb-6">
+        <h3 className="text-white font-semibold text-sm">current project: </h3>
+        <p className="border-2 font rounded-full h-fit w-fit text-red-400 border-red-400 px-4 py-0 ml-2 text-sm">
+          PAD Website
         </p>
       </div>
     </div>
