672949f2b20fa9a4ca761df5e06035075035ceb5
DannielK <dannykm003@gmail.com>
2024-01-24 03:07:14 -0800

    Created Title component (unsure about margins)

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,10 @@
-const Title = () => {
-  return <div className="">Title</div>;
+const Title = ({ title }) => {
+  return (
+    <div className="flex justify-start items-center">
+      <p className="text-2xl text-white">{title}</p>
+    </div>
+    
+  );
 };
 
 export default Title;
