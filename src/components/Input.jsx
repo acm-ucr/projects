12b03872b9ba51a8ee656e41d85635d93e53d4da
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-24 13:16:43 -0800

    cleanup input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,11 @@
 "use client";
-import React, { useState } from "react";
-
-const Input = ({ initial, onChange }) => {
-  const [current, editText] = useState(initial);
-
-  const onChangeWrapper = (e) => {
-    editText(e.target.value);
-  };
-
+const Input = ({ value, setValue }) => {
   return (
     <input
       type="text"
       className="w-full py-0.5 px-1.5 text-white rounded-md bg-projects-black focus:bg-projects-gray focus:outline-none"
-      value={current}
-      onChange={onChangeWrapper}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
     />
   );
 };
