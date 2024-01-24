4963a51f6a28851a7a4e4362d7b6b0d87a26f263
allisonlee572 <121532627+allisonlee572@users.noreply.github.com>
2024-01-24 15:10:40 -0800

    removed comments

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,6 @@ import Tag from "@/components/Tag";
 const Home = () => {
   return (
     <>
-      {/* <Tag text = "Python" color= "purple" borderStyle = "dotted"/>
-  <Tag text = "Javascript" color= "purple" />
-  <Tag text = "C#" color= "purple" />
-  <Tag text = "react.js" color= "blue" />
-  <Tag text = "tailwind css" color= "blue" />
-  <Tag text = "C++" color= "purple" />
-  <Tag text = "Git" color= "red" />
-  <Tag text = "firebase" color= "red" />
-  <Tag text = "Java" color= "purple" />
-  <Tag text = "Ruby" color= "purple" />
-
-  <Tag text = "Swift" color= "purple" />
-  <Tag text = "TypeScript" color= "purple" />
-  <Tag text = "PHP" color= "purple" />
-  <Tag text = "Dart" color= "purple" />
-  <Tag text = "Go" color= "purple" />  */}
-
       <Tag text="Javascript" color="red" />
     </>
   );
