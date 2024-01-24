40eb9a928b0090f6c42562360d3dd6890efc113c
Nimrah Saleem <nimsaleem44@gmail.com>
2024-01-24 15:26:40 -0800

    tile component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
+import Tile from "@/components/Tile";
+
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      <Tile
+        title="ACM Designer "
+        button="view"
+        hint="interview not scheduled"
+        color="projects-red"
+      />{" "}
+    </>
+  );
 };
 
 export default Home;
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,5 +1,16 @@
 const Tile = ({ title, button, subtitle, hint, color }) => {
-  return <div>Tile</div>;
+  return (
+    <>
+      <div className="font-extrabold text-orange-500"> {hint} </div>
+
+      <div className="bg-orange-500 w-fit p-4">
+        <div>{title} </div>
+        <div className="p-2 border-4 border-width "> {button}</div>
+      </div>
+    </>
+  );
 };
+
+// padding, margin, background, text
 
 export default Tile;
