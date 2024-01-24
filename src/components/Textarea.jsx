3833e7f0ccad6b8c595c027c7d6dcaab1b02afc2
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-24 12:58:42 -0800

    fix

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,20 +1,10 @@
 "use client";
-import React, { useState } from "react";
-// initialValue: string
-// onChange: (value: string) => void
-const Textarea = ({ initialValue, onChange }) => {
-  const [value, setValue] = useState(initialValue);
-
-  const onChangeWrapper = (e) => {
-    setValue(e.target.value);
-    // onChange(e.target.value);
-  };
-
+const Textarea = ({ value, setValue }) => {
   return (
     <textarea
       class="w-full m-3 p-3 bg-projects-black focus:bg-projects-gray text-white focus:outline-none rounded-xl"
       value={value}
-      onChange={onChangeWrapper}
+      onChange={(e) => setValue(e.target.value)}
     />
   );
 };
