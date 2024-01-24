ac383d14ceb6d3a59d87b076e48847dd872cccea
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-24 13:15:17 -0800

    revert

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,20 +2,20 @@
 import React, { useState } from "react";
 
 const Input = ({ initial, onChange }) => {
-    const [current, editText] = useState(initial);
+  const [current, editText] = useState(initial);
 
-    const onChangeWrapper = (e) => {
-        editText(e.target.value);
-    }
+  const onChangeWrapper = (e) => {
+    editText(e.target.value);
+  };
 
-    return (
-        <input
-            type="text"
-            className="w-full py-0.5 px-1.5 text-white rounded-md bg-projects-black focus:bg-projects-gray focus:outline-none"
-            value={current}
-            onChange={onChangeWrapper}
-        />
-    );
+  return (
+    <input
+      type="text"
+      className="w-full py-0.5 px-1.5 text-white rounded-md bg-projects-black focus:bg-projects-gray focus:outline-none"
+      value={current}
+      onChange={onChangeWrapper}
+    />
+  );
 };
 
 export default Input;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,7 @@
-const Title = ({ title }) => {
-  return (
-    <div className="flex justify-start items-center">
-      <p className="text-2xl text-white">{title}</p>
-    </div>
-    
-  );
+import React from "react";
+
+const Title = () => {
+  return <div>Title</div>;
 };
 
 export default Title;
