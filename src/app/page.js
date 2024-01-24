9b3b1f8ed0f377d0157924d004fa2cd387e9edb9
allisonlee572 <121532627+allisonlee572@users.noreply.github.com>
2024-01-24 14:57:00 -0800

    add tag component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
-const Home = () => {
-  return <>Hello World</>;
-};
+import Tag from "@/components/Tag";
 
+const Home = () => {
+  return (
+    <>
+      {/* <Tag text = "Python" color= "purple" borderStyle = "dotted"/>
+  <Tag text = "Javascript" color= "purple" />
+  <Tag text = "C#" color= "purple" />
+  <Tag text = "react.js" color= "blue" />
+  <Tag text = "tailwind css" color= "blue" />
+  <Tag text = "C++" color= "purple" />
+  <Tag text = "Git" color= "red" />
+  <Tag text = "firebase" color= "red" />
+  <Tag text = "Java" color= "purple" />
+  <Tag text = "Ruby" color= "purple" />
+
+  <Tag text = "Swift" color= "purple" />
+  <Tag text = "TypeScript" color= "purple" />
+  <Tag text = "PHP" color= "purple" />
+  <Tag text = "Dart" color= "purple" />
+  <Tag text = "Go" color= "purple" />  */}
+
+      <Tag text="Javascript" color="red" />
+    </>
+  );
+};
 export default Home;
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,5 +1,27 @@
+const colors = {
+  green: {
+    bg: "bg-green-500",
+  },
+  blue: {
+    bg: "bg-blue-500",
+  },
+  purple: {
+    bg: "bg-purple-500",
+  },
+  orange: {
+    bg: "bg-orange-500",
+  },
+  red: {
+    bg: "bg-red-500",
+  },
+};
+
 const Tag = ({ text, color }) => {
-  return <div>Tag</div>;
+  return (
+    <div className={`${colors[color].bg} w-fit rounded-full px-3 py-2`}>
+      {text}
+    </div>
+  );
 };
 
 export default Tag;
