fc29dd7347b088531f076419e830be50180b8575
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-30 13:18:43 -0800

    cleanup nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,71 +11,53 @@ import {
 
 const Navigation = () => {
   const pathname = usePathname();
-  const segments = pathname.split("/");
-  const textColor =
-    segments[1] === "developer"
-      ? "text-projects-blue"
-      : segments[1] === "lead"
-      ? "text-projects-red"
-      : "text-white";
-  const isLinkActive = (path) => pathname.startsWith(`/${segments[1]}/${path}`);
+
+  const links = {
+    developer: [
+      {
+        name: "Profile",
+        link: "/developer/profile",
+        Icon: MdPerson,
+      },
+      {
+        name: "Applications",
+        link: "/developer/applications",
+        Icon: MdOutlineListAlt,
+      },
+      {
+        name: "Forms",
+        link: "/developer/forms",
+        Icon: MdOutlineCalendarMonth,
+      },
+      {
+        name: "Interviews",
+        link: "/developer/interviews",
+        Icon: MdOutlineListAlt,
+      },
+    ],
+  };
 
   const user = pathname.includes("developer") ? "developer" : "admin";
 
   return (
     <div className="flex flex-col items-center bg-projects-black text-white text-xl font-light w-2/12 p-4">
       <Image src={logo} alt="ACM logo" className="w-1/2" />
-      <p
-        className={`rounded py-1 w-full text-center font-bold ${textColor} bg-projects-gray`}
-      >
+      <p className="rounded py-1 my-2 w-full text-center font-bold text-projects-blue bg-projects-gray">
         {user}
       </p>
-      <Link
-        href={`/${user}/profile`}
-        className="mb-4 flex items-center w-full rounded hover:bg-projects-gray"
-      >
-        <MdPerson className="text-2xl" />
-        PROFILE
-      </Link>
-      <Link
-        href={`/${user}/applications`}
-        className="mb-4 flex items-center space-x-4"
-      >
-        <MdOutlineListAlt className="text-2xl" />
-        <span
-          className={`hover:underline ${
-            isLinkActive("applications") ? textColor : ""
+
+      {links[user].map(({ name, link, Icon }, index) => (
+        <Link
+          key={index}
+          href={link}
+          className={`flex items-center w-full rounded hover:bg-projects-gray px-4 py-1 my-1 ${
+            pathname === link && "text-projects-blue"
           }`}
         >
-          APPLICATIONS
-        </span>
-      </Link>
-      <Link
-        href={`/${user}/forms`}
-        className="mb-4 flex items-center space-x-4"
-      >
-        <MdOutlineCalendarMonth className="text-2xl" />
-        <span
-          className={`hover:underline ${
-            isLinkActive("forms") ? textColor : ""
-          }`}
-        >
-          FORMS
-        </span>
-      </Link>
-      <Link
-        href={`/${user}/interviews`}
-        className="mb-4 flex items-center space-x-4"
-      >
-        <MdOutlineListAlt className="text-2xl" />
-        <span
-          className={`hover:underline ${
-            isLinkActive("interviews") ? textColor : ""
-          }`}
-        >
-          INTERVIEWS
-        </span>
-      </Link>
+          <Icon className="text-2xl mx-2" />
+          {name}
+        </Link>
+      ))}
 
       <div
         onClick={() => console.log("Logged out")}
